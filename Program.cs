81f4cc7b0dab4d37f76de1b05e7b4c9b0296a946
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-11 17:30:02 -0600

    Adding initial version of the project

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace PhotoSharingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var configuration = builder.Build();
+
+            var connectionString = configuration.GetConnectionString("StorageAccount");
+            string containerName = "photos";
+
+            BlobContainerClient container = new BlobContainerClient(connectionString, containerName);
+        }
+    }
+}
