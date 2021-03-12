9c4d52adfd2958360680f42888dfed5b267f4f80
Alvaro Enrique Ruano <alvaro.ruano90@outlook.com>
2021-03-11 18:04:27 -0600

    Adjust program to update multiple files to the blob storage

--- Program.cs
+++ Program.cs
@@ -22,10 +22,17 @@ namespace PhotoSharingApp
 
             container.CreateIfNotExists();
 
-            string blobName = "docs-and-friends-selfie-stick";
             string fileName = "docs-and-friends-selfie-stick.png";
-            BlobClient blobClient = container.GetBlobClient(blobName);
-            blobClient.Upload(fileName, true);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Random randomGenerator = new Random();
+                int randomNumber = randomGenerator.Next(999);
+                string blobName = String.Format("docs-and-friends-selfie-stick-{0}", randomNumber);
+            
+                BlobClient blobClient = container.GetBlobClient(blobName);
+                blobClient.Upload(fileName, true);
+            }
 
             var blobs = container.GetBlobs();
             foreach (var blob in blobs)
